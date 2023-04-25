51165e09cec8299a1eab461d0f470f86831166e6
project-vending <131410219+project-vending@users.noreply.github.com>
2023-04-25 18:36:44 -0400

    Add main.py

---
+++ recommendation_engine/ui/main.py
@@ -0,0 +1,20 @@
+python
+import streamlit as st
+
+# Set page title and favicon
+st.set_page_config(page_title="Recommendation Engine", page_icon=":heart:")
+
+# Define Streamlit app content
+def main():
+    st.title("Recommendation Engine")
+
+    # TODO: Add input widgets for user and item data, API endpoint, etc.
+    
+    # TODO: Call API endpoint with user and item data and get recommendations
+    
+    # TODO: Display recommendations using Streamlit components such as tables, plots, etc.
+
+
+if __name__ == "__main__":
+    main()
+
